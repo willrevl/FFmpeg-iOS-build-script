fc2ae61c8b556fac3b339ea4884b2dd6d37459f3
willrevl <william@revl.com>
2017-08-08 12:57:24 -0700

    use avresample

--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -14,7 +14,7 @@ THIN=`pwd`/"thin"
 #FDK_AAC=`pwd`/../fdk-aac-build-script-for-iOS/fdk-aac-ios
 
 CONFIGURE_FLAGS="--enable-cross-compile --disable-debug --disable-programs \
-                 --disable-doc --enable-pic"
+                 --disable-doc --enable-pic --enable-avresample"
 
 if [ "$X264" ]
 then
